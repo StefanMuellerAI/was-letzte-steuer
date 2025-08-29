444a3c8edb71e05e55ab3d7e1a7265ef0252ed79
DaVinciKollektiv <118380549+DaVinciKollektiv@users.noreply.github.com>
2025-08-29 20:54:32 +0200

    Simplify Next.js config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  distDir: 'out',
-  generateBuildId: () => 'build'
+  }
 }
 
 module.exports = nextConfig
